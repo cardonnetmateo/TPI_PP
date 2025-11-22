d7e32818475aa65ef7e6f093490279451156922a
TizianoEstevez <tiziestevez78@gmail.com>
2025-11-22 15:41:14 -0300

    Replace hardcoded strings with enum messages

--- Interfaz/RecargarSaldo.java
+++ Interfaz/RecargarSaldo.java
@@ -1,6 +1,8 @@
 package Interfaz;
 
 import javax.swing.*;
+
+import Enum.Mensajes;
 import Modelos.Cuenta;
 
 public class RecargarSaldo extends JFrame {
@@ -10,16 +12,16 @@ public class RecargarSaldo extends JFrame {
     private JButton btnRecargarAbonoMensual;
 
     public RecargarSaldo() {
-        setTitle("Recargar Saldo");
+        setTitle(Mensajes.RECARGAR_SALDO);
         setSize(420, 320);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(null);
 
-        JLabel titulo = new JLabel("Recargar Saldo de la Cuenta");
+        JLabel titulo = new JLabel(Mensajes.RECARGAR_SALDO);
         add(titulo);
         titulo.setBounds(100, 20, 220, 30);
 
-        JLabel lblCuenta = new JLabel("Seleccionar Cuenta:");
+        JLabel lblCuenta = new JLabel(Mensajes.SELECCIONE_CUENTA);
         add(lblCuenta);
         lblCuenta.setBounds(50, 70, 150, 25);
 
@@ -27,7 +29,7 @@ public class RecargarSaldo extends JFrame {
         add(cmbCuentas);
         cmbCuentas.setBounds(200, 70, 160, 25);
 
-        JLabel lblCantidad = new JLabel("Cantidad a recargar:");
+        JLabel lblCantidad = new JLabel(Mensajes.CANTIDAD_A_RECARGAR);
         add(lblCantidad);
         lblCantidad.setBounds(50, 120, 150, 25);
 
@@ -35,11 +37,11 @@ public class RecargarSaldo extends JFrame {
         add(txtCantidad);
         txtCantidad.setBounds(200, 120, 160, 25);
 
-        btnRecargarCuentaCorriente = new JButton("Recargar Cuenta Corriente");
+        btnRecargarCuentaCorriente = new JButton(Mensajes.RECARGAR_SALDO);
         add(btnRecargarCuentaCorriente);
         btnRecargarCuentaCorriente.setBounds(60, 170, 160, 30);
 
-        btnRecargarAbonoMensual = new JButton("Recargar Abono Mensual");
+        btnRecargarAbonoMensual = new JButton(Mensajes.RECARGAR_CUENTA_CORRIENTE);
         add(btnRecargarAbonoMensual);
         btnRecargarAbonoMensual.setBounds(220, 170, 160, 30);
     }
