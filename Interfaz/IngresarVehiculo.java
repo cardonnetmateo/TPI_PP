c7a27dd875f54996b8efed347cb0ce4f7fc70b2b
TizianoEstevez <tiziestevez78@gmail.com>
2025-11-22 15:26:31 -0300

    Replace hardcoded strings with enum messages

--- Interfaz/IngresarVehiculo.java
+++ Interfaz/IngresarVehiculo.java
@@ -1,21 +1,22 @@
 package Interfaz;
 import javax.swing.*;
 
+import Enum.Mensajes;
 import Modelos.Plaza;
 public class IngresarVehiculo extends JFrame{
     private JButton btnIngresar;
 
     public IngresarVehiculo(){
-        setTitle("Ingresar Vehículo");
+        setTitle(Mensajes.INGRESAR_VEHICULO);
         setSize(400, 300);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(null);
 
-        JLabel titulo = new JLabel("Ingresar Vehículo al Estacionamiento");
+        JLabel titulo = new JLabel(Mensajes.INGRESAR_VEHICULO);
         add(titulo);
         titulo.setBounds(80, 20, 250, 30);
 
-        JLabel lblPatente = new JLabel("Patente del Vehículo:");
+        JLabel lblPatente = new JLabel(Mensajes.PATENTE_VEHICULO);
         add(lblPatente);
         lblPatente.setBounds(50, 70, 150, 25);
 
@@ -23,7 +24,7 @@ public class IngresarVehiculo extends JFrame{
         add(txtPatente);
         txtPatente.setBounds(200, 70, 100, 25);
 
-        JLabel lblPlaza = new JLabel("Número de Plaza:");
+        JLabel lblPlaza = new JLabel(Mensajes.NUMERO_PLAZA);
         add(lblPlaza);
         lblPlaza.setBounds(50, 110, 150, 25);
 
@@ -31,7 +32,7 @@ public class IngresarVehiculo extends JFrame{
         add(cmbPlazas);
         cmbPlazas.setBounds(200, 110, 100, 25);
 
-        btnIngresar = new JButton("Ingresar Vehículo");
+        btnIngresar = new JButton(Mensajes.INGRESAR_VEHICULO);
         add(btnIngresar);
         btnIngresar.setBounds(130, 160, 150, 30);
     }
